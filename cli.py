72671a09f4290f41e3be41dff570a054f92ce905
opo <opo@opo.com>
2024-02-13 09:16:09 +1030

    Add CLI for instrument only, document future issues, update

--- cli.py
+++ cli.py
@@ -15,46 +15,65 @@ def main(
         output_format: str = typer.Option(default="WAV", help="Output directory"),
         is_gpu_conversion: bool = typer.Option(is_flag=True, default=False, help="Should use GPU to perform conversion"),
         is_vocals_only: bool = typer.Option(is_flag=True, default=False, help="Should only save vocals"),
+        is_instrumentals_only: bool = typer.Option(is_flag=True, default=False, help="Should only save instrumentals"),
 ):
-    process(input_files, output_dir, output_format, is_gpu_conversion, is_vocals_only)
+    process(input_files, output_dir, output_format, is_gpu_conversion, is_vocals_only, is_instrumentals_only)
 
 
 @app.command()
-def process(input_files: List[str], output_dir: str, output_format, is_gpu_conversion: bool, is_vocals_only: bool):
-    input_files, output_dir_path, output_format = _validate_args(input_files, output_dir, output_format)
+def process(input_files: List[str], output_dir: str, output_format, is_gpu_conversion: bool, is_vocals_only: bool, is_instrumentals_only: bool):
+    input_files, output_dir_path, output_format = _validate_args(input_files, output_dir, output_format, is_vocals_only ,is_instrumentals_only)
     should_emulate_display = True if os.getenv('UVR_emulate_display') is not None else False
     root = UVR.MainWindow(is_cli=True, should_emulate_display=should_emulate_display)
     # Need to do this, as a lot of state is global (e.g. new objects refer to state in `root` on init ...)
     UVR.set_root(root)
 
-    _apply_setting(root, input_files, output_dir_path, output_format, is_gpu_conversion, is_vocals_only)
+    _apply_setting(root, input_files, output_dir_path, output_format, is_gpu_conversion, is_vocals_only, is_instrumentals_only)
 
     root.process_start()
+    _cleanup(root)
     raise typer.Exit()
 
 
-def _apply_setting(root: UVR.MainWindow, input_files: list[str], output_dir_path: str, output_format: str, is_gpu_conversion: bool, is_vocals_only: bool):
+def _apply_setting(root: UVR.MainWindow, input_files: list[str], output_dir_path: str, output_format: str, is_gpu_conversion: bool, is_vocals_only: bool, is_instrumentals_only: bool):
     # In docker, we do not copy across any local `data.pkl` file, so we will load the
     # default settings (see `gui_data/constants.py` -> `DEFAULT_DATA`) first, then "apply" what we want.
     # Alternatively, a manually curated `data.pkl` can be made via the GUI, then mounted it and then tweaked as needed.
     root.command_Text.write(f'Loaded settings: {root.get_settings_list()}')
     root.command_Text.write(f'Applying settings from CLI ...')
 
+    # Filename on disk is `UVR-MDX-NET-Inst_HQ_1.onnx`.
+    # TODO - Inst HQ 1 has `Inst... Only` and then `Vocals Only`, but MDX23C is the opposite, so hence why
+    # it is primary vs secondary. Segment Size and Overlap also have different "bases". so CLI is going
+    # to get busy very fast (especially if want ensemble mode, which.... yikes).
     root.mdx_net_model_var.set('UVR-MDX-NET Inst HQ 1')
     # TODO - No idea how we actually select GPUs, I guess just assume always 1?
     root.is_gpu_conversion_var.set(int(is_gpu_conversion is True))
     root.save_format_var.set(output_format)
+
+    # Reset these to ensure we honour CLI args, and do not get stuck in state when loading
+    # the 'data.pkl'
+    root.is_primary_stem_only_var.set(False)
+    root.is_secondary_stem_only_var.set(False)
     if is_vocals_only:
-        root.command_Text.write(UVR.VOCAL_STEM_ONLY)
+        # TODO - Think the exact text might be model specific? And the GUI has to do this odd pattern
+        # to confirm that?
         root.is_secondary_stem_only_Text_var.set(UVR.VOCAL_STEM_ONLY)
         root.is_secondary_stem_only_var.set(True)
-    else:
-        # TODO - Confirm is this makes sense...
-        root.is_secondary_stem_only_var.set(False)
+    elif is_instrumentals_only:
+        # TODO - No idea where this string is coming from/defined
+        root.is_primary_stem_only_Text_var.set('Instrumental Only')
+        root.is_primary_stem_only_var.set(True)
     root.command_Text.write(f'Resulting settings for conversion: {root.get_settings_list()}')
 
     _set_input(input_files, root)
     root.export_path_var.set(output_dir_path)
+
+
+def _cleanup(root: UVR.MainWindow):
+    root.is_primary_stem_only_var.set(False)
+    root.is_secondary_stem_only_var.set(False)
+    root.save_values()
 
 
 def _set_input(input_files: list[str], root: UVR.MainWindow):
@@ -64,7 +83,7 @@ def _set_input(input_files: list[str], root: UVR.MainWindow):
 
 
 # TODO - ... fairly sure can hook this to Typer directly?
-def _validate_args(input_files: List[str], output_dir: str, output_format: str):
+def _validate_args(input_files: List[str], output_dir: str, output_format: str, is_vocals_only: bool, is_instrumentals_only: bool):
     output_dir_path = os.path.join(output_dir)
     if not os.path.isdir(output_dir_path):
         typer.echo(f"'--output-dir' argument [{output_dir}] is not a directory")
@@ -84,6 +103,10 @@ def _validate_args(input_files: List[str], output_dir: str, output_format: str):
 
     if output_format not in SUPPORTED_OUTPUT_FORMATS:
         typer.echo(f"'--output-format' argument [{output_format}] not supported, must be one of {SUPPORTED_OUTPUT_FORMATS}")
+        raise typer.Exit()
+
+    if is_instrumentals_only and is_vocals_only:
+        typer.echo(f"'Cannot run with both '--is-vocals-only' and '--is-instrumentals-only'; these options are mutually exclusive")
         raise typer.Exit()
 
     return input_files, output_dir_path, output_format
